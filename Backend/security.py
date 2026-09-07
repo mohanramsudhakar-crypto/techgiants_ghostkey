ab8c7c2c630b8541a8019a94b998f72d05c1680d
mohanramsudhakar-crypto <mohanramsudhakar@gmail.com>
2026-09-07 12:16:10 +0530

    Add files via upload

---
+++ Backend/security.py
@@ -0,0 +1,21 @@
+import hashlib
+import hmac
+
+
+def calculate_hmac(secret, message):
+    return hmac.new(
+        secret.encode(),
+        message.encode(),
+        hashlib.sha256
+    ).hexdigest()
+
+
+def verify_hmac(secret, message, received):
+    expected = calculate_hmac(secret, message)
+
+    return hmac.compare_digest(
+        expected,
+        received
+    )
+
+print(10)
