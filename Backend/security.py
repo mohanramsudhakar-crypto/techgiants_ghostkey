06a1aa890ac6db200d4ebcfd4faf7fd17a64f9af
mdraihanstar2009-hash <mdraihanstar2009@gmail.com>
2026-09-08 00:36:56 +0530

    Update security.py

--- Backend/security.py
+++ Backend/security.py
@@ -4,21 +4,19 @@ import hmac
 
 def hmac_sha256(secret, message):
     return hmac.new(
-        secret.encode(),
-        message.encode(),
+        secret.encode("utf-8"),
+        message.encode("utf-8"),
         hashlib.sha256
     ).hexdigest()
 
 
 def verify_hmac(secret, message, received_hmac):
-    
-    expected = hmac_sha256(
-        secret,
-        message
-    )
+    if not received_hmac:
+        return False
+
+    expected = hmac_sha256(secret, message)
 
     return hmac.compare_digest(
         expected,
         received_hmac
     )
-
